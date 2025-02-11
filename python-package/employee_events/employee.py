3adf0f0bc33825282a1f885472ebab4f18d0b28c
Peter Nguyen <peter.t.nguyen8@gmail.com>
2025-02-11 18:18:28 -0500

    update employee_events package

--- python-package/employee_events/employee.py
+++ python-package/employee_events/employee.py
@@ -1,24 +1,24 @@
 # Import the QueryBase class
-#### YOUR CODE HERE
+from query_base import QueryBase
 
 # Import dependencies needed for sql execution
 # from the `sql_execution` module
-#### YOUR CODE HERE
+import pandas as pd
 
 # Define a subclass of QueryBase
 # called Employee
-#### YOUR CODE HERE
+class Employee(QueryBase):
 
     # Set the class attribute `name`
     # to the string "employee"
-    #### YOUR CODE HERE
+    name = "employee"
 
 
     # Define a method called `names`
     # that receives no arguments
     # This method should return a list of tuples
     # from an sql execution
-    #### YOUR CODE HERE
+    def names(self):
         
         # Query 3
         # Write an SQL query
@@ -27,14 +27,15 @@
         # 2. The employee's id
         # This query should return the data
         # for all employees in the database
-        #### YOUR CODE HERE
+        query = f"SELECT full_name, id FROM {self.name}"
+        return self.query(query)
     
 
     # Define a method called `username`
     # that receives an `id` argument
     # This method should return a list of tuples
     # from an sql execution
-    #### YOUR CODE HERE
+    def username(self, id):
         
         # Query 4
         # Write an SQL query
@@ -42,7 +43,9 @@
         # Use f-string formatting and a WHERE filter
         # to only return the full name of the employee
         # with an id equal to the id argument
-        #### YOUR CODE HERE
+        query = f"SELECT full_name FROM {self.name} WHERE id = {id}"
+        return self.query(query)
+        
 
 
     # Below is method with an SQL query
@@ -50,12 +53,10 @@
     # the machine learning model.
     # Without editing the query, alter this method
     # so when it is called, a pandas dataframe
-    # is returns containing the execution of
+    # is returned containing the execution of
     # the sql query
-    #### YOUR CODE HERE
     def model_data(self, id):
-
-        return f"""
+        query = f"""
                     SELECT SUM(positive_events) positive_events
                          , SUM(negative_events) negative_events
                     FROM {self.name}
@@ -63,3 +64,5 @@
                         USING({self.name}_id)
                     WHERE {self.name}.{self.name}_id = {id}
                 """
+        result = self.pandas_query(query)
+        return result
--- tests/test.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from pathlib import Path
-
-# Using pathlib create a project_root
-# variable set to the absolute path
-# for the root of this project
-project_root = Path(__file__).parent.parent
-print('this is the root:', project_root)
