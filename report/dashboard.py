e81b3d59a7f7d4cb512833e6ce8417338d6017d4
Peter Nguyen <peter.t.nguyen8@gmail.com>
2025-02-12 23:47:22 -0500

    update dashboard.py and utils.py

--- report/dashboard.py
+++ report/dashboard.py
@@ -2,10 +2,10 @@ from fasthtml.common import *
 import matplotlib.pyplot as plt
 
 # Import QueryBase, Employee, Team from employee_events
-#### YOUR CODE HERE
+from employee_events import QueryBase, Employee, Team
 
 # import the load_model function from the utils.py file
-#### YOUR CODE HERE
+from .utils import load_model
 
 """
 Below, we import the parent classes
@@ -24,28 +24,31 @@ from combined_components import FormGroup, CombinedComponent
 
 # Create a subclass of base_components/dropdown
 # called `ReportDropdown`
-#### YOUR CODE HERE
+class ReportDropdown(Dropdown):
     
     # Overwrite the build_component method
     # ensuring it has the same parameters
     # as the Report parent class's method
-    #### YOUR CODE HERE
+    def build_component(self, entity_id, model):
+        
         #  Set the `label` attribute so it is set
         #  to the `name` attribute for the model
-        #### YOUR CODE HERE
+        self.label = model.name
         
         # Return the output from the
         # parent class's build_component method
-        #### YOUR CODE HERE
+        return super().build_component(model)
     
     # Overwrite the `component_data` method
     # Ensure the method uses the same parameters
     # as the parent class method
-    #### YOUR CODE HERE
+    def component_data(self, entity_id, model):
         # Using the model argument
         # call the employee_events method
         # that returns the user-type's
         # names and ids
+        
+
 
 
 # Create a subclass of base_components/BaseComponent
@@ -164,6 +167,9 @@ from combined_components import FormGroup, CombinedComponent
         ax.barh([''], [pred])
         ax.set_xlim(0, 1)
         ax.set_title('Predicted Recruitment Risk', fontsize=20)
+
+    def new_method(self, entity_id, model):
+        return model.get_names_and_ids(entity_id)
         
         # pass the axis variable
         # to the `.set_axis_styling`
